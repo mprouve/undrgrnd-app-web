20d833f3e04d85f0c5d663a51e8e2533f12294ae
Marco Prouve <marcoprouve@gmail.com>
2022-08-06 18:57:50 -0700

    Added glitch animation ot landing page

--- src/components/main/components/landing/landing.tsx
+++ src/components/main/components/landing/landing.tsx
@@ -6,13 +6,22 @@ import {
   // SecondaryName,
   // PrimaryName,
 } from "./styled-components/landing"
+import "./scss/glitch-2.scss"
 
 const Landing = (): JSX.Element => {
   return (
     <MainContainer
-      style={{
-        backgroundImage: `url(https://res.cloudinary.com/marco-prouve/image/upload/v1659817984/undrgrnd/backgrounds/graffiti-bw_hdrsl9.webp)`,
-      }}>
+      className="glitch"
+      // style={{
+      //   backgroundImage: `url(https://res.cloudinary.com/marco-prouve/image/upload/v1659817984/undrgrnd/backgrounds/graffiti-bw_hdrsl9.webp)`,
+      // }}
+    >
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+      <div className="glitch__item" />
+
       <TransparentOverlay>
         <LogoImg
           src="https://res.cloudinary.com/marco-prouve/image/upload/v1659820387/undrgrnd/logo/undrgrnd-logo-transparent-1300-641_x8f7zn.webp"
@@ -21,6 +30,7 @@ const Landing = (): JSX.Element => {
           height="641"
           loading="lazy"
         />
+
         {/* <Name>
           <PrimaryName className="glitch-landing" data-text="UNDRGRND">
             <SecondaryName className="glitch-landing" data-text="THE">
